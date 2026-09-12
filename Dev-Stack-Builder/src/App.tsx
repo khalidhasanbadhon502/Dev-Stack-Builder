9d3eb76928d36523d907ca8539a7dae182c7c707
khalidhasanbadhon502 <khalid505hasan505@gmail.com>
2026-09-12 13:56:51 +0600

    add footer component

--- Dev-Stack-Builder/src/App.tsx
+++ Dev-Stack-Builder/src/App.tsx
@@ -1,15 +1,19 @@
 import  Nav  from './components/Nav'
 import Banner from './components/Banner'
 import {TechExplorer} from './components/TechExplorer'
+import { Footer } from './components/Footer';
 
 function App() {
   
 
   return (
     <div className="min-h-screen bg-white">
+      <div>
       <Nav />
       <Banner />
-      <TechExplorer />
+        <TechExplorer />
+      </div>
+      <Footer />
     </div>
   )
 }
